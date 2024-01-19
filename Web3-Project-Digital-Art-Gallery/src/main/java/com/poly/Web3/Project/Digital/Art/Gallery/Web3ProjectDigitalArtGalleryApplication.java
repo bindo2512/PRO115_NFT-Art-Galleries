811f4c9c999cd062cb4fa275847df2835347206a
Sang <bindo2512@gmail.com>
2024-01-19 18:13:45 +0700

    Initial Commit

---
+++ Web3-Project-Digital-Art-Gallery/src/main/java/com/poly/Web3/Project/Digital/Art/Gallery/Web3ProjectDigitalArtGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.poly.Web3.Project.Digital.Art.Gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web3ProjectDigitalArtGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web3ProjectDigitalArtGalleryApplication.class, args);
+	}
+
+}
---
+++ Web3-Project-Digital-Art-Gallery/src/test/java/com/poly/Web3/Project/Digital/Art/Gallery/Web3ProjectDigitalArtGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.Web3.Project.Digital.Art.Gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web3ProjectDigitalArtGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
